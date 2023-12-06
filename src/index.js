5d27724897ed0c71a4e9c840818d52b279036abb
mfn <2976908894@qq.com>
2023-12-06 18:12:49 +0800

    局部主题配置

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import {RouterProvider} from "react-router-dom";
 // 1.导入路由Router
 import router from "./router";
+// 导入主题文件
+import "./theme.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,10 +1,16 @@
 import {Outlet} from "react-router-dom";
+import {Button} from "antd-mobile";
 
 const Layout = () => {
     return (
 
         <div>
-            这是Layout一级路
+            这是Layout一级路由
+            {/*测试全局样式*/}
+            <Button color="primary">测试全局</Button>
+            <div className="purple">
+                <Button color="primary">测试局部</Button>
+            </div>
             <Outlet/>
         </div>
     )
